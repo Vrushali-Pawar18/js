2921034e83c3b26ce8dab164467656c374f62db7
Vrushali-Pawar18 <vrushalipawar1802@gmail.com>
2024-02-22 18:25:39 +0530

    added objects

---
+++ objects.js
@@ -0,0 +1,26 @@
+const person = {
+    fname : "Vrushali",
+    age : 23,
+    hobbies : ["a","b","c"]
+}
+
+// console.log(person.age);
+// person.gender = "female"; 
+// console.log(person);
+// console.log(person.hobbies);
+
+// //bracket notations
+// console.log(person["fname"]);
+
+//to iterate in objects we use:
+//1) for in loop
+//2) object.keys
+
+// for(key in person){
+//     console.log(person[key]);
+// }
+
+//Object.keys gives us the array of keys present in the oobject.
+for(let key of Object.keys(person)){
+    console.log(person[key]);
+}
